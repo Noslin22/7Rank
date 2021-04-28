ca1ec4c924f25563f9d042d70b11b708760dd18a
João nilson <joaonilsoneto2@gmail.com>
2021-04-28 13:30:58 -0300

    and again

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "assets/NOTICES": "1977e1e3d18842aede18620d0d837f65",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "9ab370029fd7a5e5b0a11f572572f0a9",
-"/": "9ab370029fd7a5e5b0a11f572572f0a9",
+"index.html": "682c094dc7db4924f9b32d8a33f301fb",
+"/": "682c094dc7db4924f9b32d8a33f301fb",
 "main.dart.js": "3229d20beab2656b4342735c1d79f719",
 "manifest.json": "1726bc90bb76dd3cecf06dc8c35b881c",
 "version.json": "4639302361f56de9af8af6013d368ab3"
